13bbc6ab6bf98867573af4ce7594b3ef9b9b2541
Lê Gia Hoàng <giahoangth@gmail.com>
2018-06-15 09:39:01 +0700

    first commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+const { graphql, buildSchema } = require('graphql');
+
+let schema = buildSchema(`
+  type Query {
+    mew: String
+  }
+`);
+
+const root = { mew: () => 'Hello world!' };
+
+graphql(schema, '{ mew }', root).then((response) => {
+  console.log(response);
+});
