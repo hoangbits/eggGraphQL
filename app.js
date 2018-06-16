80e669b14310a7a657334d7fcba5a5293b53f8e4
Lê Gia Hoàng <giahoangth@gmail.com>
2018-06-16 17:02:46 +0700

    call APIs that return different types

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-    type Query {
-        hello: String
-    } 
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
 `);
-// The root provides a resolver function for each API Endpoint
+
+// The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
-      return 'hi yaaaaaa!';
-  },   
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
 };
 
 const app = express();
-app.use('/graphql',graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
 }));
-app.listen(3000);
-console.log('Running a GraphQL API Server at localhost:3000/graphql');
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
